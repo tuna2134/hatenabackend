ce35afc321fee3cc5c3253f4d7a485ebf9a54c63
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-10 19:37:27 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from sanic import Sanic
+from importlib import import_module
+
+app = Sanic("hatenabot")
+
+for name in listdir("./blueprints"):
+    if not name.startswith("_"):
+        module = import_module(f"blueprints.{name[:-3]}")
+        if hasattr(module, "bp"):
+            module.bp.app = app
+            app.blueprint(module.bp)
+                                                  
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 8000)
