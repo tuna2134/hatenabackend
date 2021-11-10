4cc6a8a63fc2d5f287a5fc54b8272f51b41f609a
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-10 22:28:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from sanic import Sanic
 from importlib import import_module
 from os import listdir
+from jinja2 import Environment, FileSystemLoader
+from flask_misaka import Misaka
+from sanic.response import html
 
 app = Sanic("hatenabot")
 
 @app.listener("before_server_start")
 async def setup(app, loop):
+    app.ctx.env = Environment(loader=FileSystemLoader('./templates/', encoding='utf8'), enable_async=True)
+    app.ctx.env.filters.setdefault("markdown", Misaka(autolink=True))
+    async def template(tpl, **kwargs):
+        template = env.get_template(tpl)
+        return html(await template.render_async(kwargs))
+    app.ctx.template = template
     app.ctx.pool = await aiomysql.create_pool(host="public-cbsv1.net.rikusutep.xyz", user="dms", password="dms", loop=loop, db="b3vad_rtbot", autocommit=True)
 
 for name in listdir("./blueprints"):
