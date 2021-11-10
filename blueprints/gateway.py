270ff271934a138008805b430b3177695bd7db08
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-10 20:41:34 +0900

    Update gateway.py

--- blueprints/gateway.py
+++ blueprints/gateway.py
@@ -5,14 +5,14 @@ bp = Blueprint("gateway")
 
 @bp.websocket("/gateway")
 async def gateway(request, ws):
-    await ws.send(dumps{
+    await ws.send(dumps({
         "t": "hello"
-    })
+    }))
     while True:
         data = loads(await ws.recv())
         if data["t"] == "login":
             if data["token"] == "questiongateway":
                 bp.app.wslist.append(ws)
-                await ws.send({
+                await ws.send(dumps({
                     "t": "login"
-                })
+                }))
