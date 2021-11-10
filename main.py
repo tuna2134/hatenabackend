b74f6e2b2829089f77e19da1bd3964447414736d
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-10 22:22:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from os import listdir
 
 app = Sanic("hatenabot")
 
+@app.listener("before_server_start")
+async def setup(app, loop):
+    app.ctx.pool = await aiomysql.create_pool(host="public-cbsv1.net.rikusutep.xyz", user="dms", password="dms", loop=loop, db="b3vad_rtbot", autocommit=True)
+
 for name in listdir("./blueprints"):
     if not name.startswith("_"):
         module = import_module(f"blueprints.{name[:-3]}")
