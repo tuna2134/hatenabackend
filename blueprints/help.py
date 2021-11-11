bea088a42d9a99e0543e4ba369bcd4190c843109
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-11 21:21:00 +0900

    Update help.py

--- blueprints/help.py
+++ blueprints/help.py
@@ -6,12 +6,10 @@ bp = Blueprint("help")
 
 @bp.route("/help")
 async def help_show(request):
-    print(data)
     return await bp.app.ctx.template("help.html", data = data)
 
 @bp.route("/api/help", methods = ["POST"])
 async def help_setting(request):
-    print(request.json)
     global data
     data = request.json
     return json({
