a8e88e2ab835eddbb7a347957b5d95401bf79352
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-11 21:54:31 +0900

    Update help.py

--- blueprints/help.py
+++ blueprints/help.py
@@ -12,6 +12,10 @@ async def help_show(request):
 async def help_category(request, category):
     return await bp.app.ctx.template("help_category.html", category=category, data=data[category])
 
+@bp.route("/help/<category>/<cmd>")
+async def help_command(request, category, cmd):
+    return await self.bot.template("help_command.html", cmd=cmd), data=self.data[category]["command"][cmd][1])
+
 @bp.route("/api/help", methods = ["POST"])
 async def help_setting(request):
     global data
