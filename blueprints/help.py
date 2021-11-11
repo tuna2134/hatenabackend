20fc1f17af82bcdfb72de0e6b12c2a26fe86cdb4
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-11 20:45:05 +0900

    Update help.py

--- blueprints/help.py
+++ blueprints/help.py
@@ -3,6 +3,11 @@ from sanic.response import json
 
 bp = Blueprint("help")
 
+@bp.route("/help")
+async def help_show(request):
+    data = bp.app.ctx.help
+    return await bp.app.ctx.template("help.html", data = data)
+
 @bp.route("/api/help", methods = ["POST"])
 async def help_setting(request):
     bp.app.ctx.help = request.json
