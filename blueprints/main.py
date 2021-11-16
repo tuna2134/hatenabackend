5e99aeafe76bf7babd280d957c6504839b3d8621
tnt <45692264+tuna2134@users.noreply.github.com>
2021-11-17 07:50:35 +0900

    Update main.py

--- blueprints/main.py
+++ blueprints/main.py
@@ -1,8 +1,12 @@
 from sanic import Blueprint
-from sanic.response import text
+from sanic.response import redirect
 
 bp = Blueprint("main")
 
 @bp.route("/")
 async def main(request):
     return await bp.app.ctx.template("index.html")
+
+@bp.route("/favicon.ico")
+async def favicon(request):
+    return redirect("/static/image/favicon.ico")
